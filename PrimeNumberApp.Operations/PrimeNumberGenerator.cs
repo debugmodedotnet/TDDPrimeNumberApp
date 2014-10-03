d4038614c8b5a1814981389cb402236ceb1555b0
debugmode@outlook.com <debugmode@outlook.com>
2014-10-03 19:33:08 +0530

    Created the project structure

---
+++ PrimeNumberApp.Operations/PrimeNumberGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrimeNumberApp.Core;
+
+namespace PrimeNumberApp.Operations
+{
+    public class PrimeNumberGenerator  : IPrimeNumberGenerator
+    {
+
+        
+
+        public IEnumerable<int> GetNumbers(int count)
+        {
+            //List<int> lstNumbers = new List<int>();
+            //lstNumbers.Add(1);
+            
+            //for(int i = 2;i<=count; i++)
+            //{
+
+
+            //}
+
+            return null;
+        }
+    }
+}
---
+++ PrimeNumberApp.Tests/PrimeNumberGeneratorClassShould.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimeNumberApp.Tests
+{
+    [TestClass]
+    public class PrimeNumberGeneratorClassShould
+    {
+        [TestMethod]
+        public void ReturnsExpectedCount()
+        {
+
+        }
+
+    }
+}
