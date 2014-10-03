1d42a5ac263fb84b0be161729a2692b452706985
debugmode@outlook.com <debugmode@outlook.com>
2014-10-03 20:59:07 +0530

    Implemented Console Application

--- PrimeNumberApp/Program.cs
+++ PrimeNumberApp/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PrimeNumberApp.Operations;
+using PrimeNumberApp.Core;
+
 
 namespace PrimeNumberApp
 {
@@ -10,6 +13,27 @@ namespace PrimeNumberApp
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Enter Limit to find Prime numbers");
+            int limit = Convert.ToInt32(Console.ReadLine());
+            if (limit < 1)
+            {
+                Console.WriteLine("Enter number greater than 0");
+            }
+            else
+            {
+
+                IPrimeNumberGenerator _primeNumberGenerator = new PrimeNumberGenerator();
+                var result = _primeNumberGenerator.GetNumbers(limit);
+                Console.WriteLine("Total number of Prime Numbers : " + result.Count());
+                foreach(var r in result)
+                {
+                    Console.Write(r + ",");
+                }
+                Console.ReadKey(true);
+            }
+
+            
         }
     }
 }
