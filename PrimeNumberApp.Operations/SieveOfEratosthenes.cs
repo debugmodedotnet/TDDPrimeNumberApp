19aeff3b5b720aee7e8011d2640d7ded0d22e48f
debugmode@outlook.com <debugmode@outlook.com>
2014-10-06 17:49:30 +0530

    Improved User experince in Console Application

---
+++ PrimeNumberApp.Operations/SieveOfEratosthenes.cs
@@ -0,0 +1,31 @@
+﻿using PrimeNumberApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberApp.Operations
+{
+   public class SieveOfEratosthenes : ITestPrime
+    {
+      
+        PrimeNumberResult ITestPrime.FindPrimeNumbers(int MaxLimit)
+        {
+            int current = 1;
+            PrimeNumberResult result = new PrimeNumberResult();
+            var pc = Enumerable.Range(2, MaxLimit).ToList();
+            while (current <= Math.Sqrt(MaxLimit))
+            {
+               // counter++;
+                current = pc.First(i => i > current);
+                pc.RemoveAll(i => i != current && i % current == 0);
+            }
+
+            result.PrimeNumbers = pc;
+            result.NumberOfOperations = 0;
+            return result; 
+
+        }
+    }
+}
--- PrimeNumberApp/Program.cs
+++ PrimeNumberApp/Program.cs
@@ -13,44 +13,48 @@ namespace PrimeNumberApp
         static void Main(string[] args)
         {
 
-            Stopwatch sw = new Stopwatch();
-            PrimeNumberGenerator _primeNumberGenerator = null; 
-            Console.WriteLine("Enter Limit to find Prime numbers");
-            int limit = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Press 1 to find Prime numbers using Divideby PrimeNumbers in Loop algorithm");
-            Console.WriteLine("Press 2 to find Prime numbers using sieve of eratosthenes algorithm");
-            int option = Convert.ToInt32(Console.ReadLine().Trim());
-            if(option ==1)
+            Console.WriteLine("Press ESC to Exit");
+            do
             {
-                _primeNumberGenerator = new PrimeNumberGenerator(new DivideByPrimeNumbersInLoopMethod());
-            }
-            else if(option ==2)
-            {
-                _primeNumberGenerator = new PrimeNumberGenerator(new SieveOfEratosthenes());
-            }
-            else
-            {
-                Console.WriteLine("wrong selection !");
+                Stopwatch sw = new Stopwatch();
+                PrimeNumberGenerator _primeNumberGenerator = null;
+                Console.WriteLine("Enter Limit to find Prime numbers");
+                int limit = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Press 1 to find Prime numbers using Divideby PrimeNumbers in Loop algorithm");
+                Console.WriteLine("Press 2 to find Prime numbers using sieve of eratosthenes algorithm");
+                int option = Convert.ToInt32(Console.ReadLine().Trim());
+                if (option == 1)
+                {
+                    _primeNumberGenerator = new PrimeNumberGenerator(new DivideByPrimeNumbersInLoopMethod());
+                }
+                else if (option == 2)
+                {
+                    _primeNumberGenerator = new PrimeNumberGenerator(new SieveOfEratosthenes());
+                }
+                else
+                {
+                    Console.WriteLine("wrong selection !");
 
-            }
+                }
 
-            if (limit < 1)
-            {
-                Console.WriteLine("Enter number greater than 0");
-            }
-            else
-            {
-                sw.Reset();
-                sw.Start();         
-                var result = _primeNumberGenerator.GetNumbers(limit);
-                Console.WriteLine("Total number of Operations : " + result.NumberOfOperations);
-                Console.WriteLine("Total number of Prime Numbers : " + result.PrimeNumbers.Count());
-                var resultToPrint =  string.Join(",", result.PrimeNumbers.Select(n => n.ToString()).ToArray());
-                Console.WriteLine(resultToPrint);
-                sw.Stop();              
-                Console.WriteLine("Time taken={0}", sw.Elapsed);
-                Console.ReadKey(true);
-            }
+                if (limit < 1)
+                {
+                    Console.WriteLine("Enter number greater than 0");
+                }
+                else
+                {
+                    sw.Reset();
+                    sw.Start();
+                    var result = _primeNumberGenerator.GetNumbers(limit);
+                    Console.WriteLine("Total number of Operations : " + result.NumberOfOperations);
+                    Console.WriteLine("Total number of Prime Numbers : " + result.PrimeNumbers.Count());
+                    var resultToPrint = string.Join(",", result.PrimeNumbers.Select(n => n.ToString()).ToArray());
+                    Console.WriteLine(resultToPrint);
+                    sw.Stop();
+                    Console.WriteLine("Time taken={0}", sw.Elapsed);
+                    Console.ReadKey(true);
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 
         }
     }
